4c25b4ab122ab2280b04dadadc9bc1000d03db2d
runner <runner@Mac-1600349780114.local>
2020-09-17 13:39:53 +0000

    dark-notify 0.1.0

--- Formula/dark-notify.rb
+++ Formula/dark-notify.rb
@@ -1,9 +1,9 @@
 class DarkNotify < Formula
   desc "Watcher for macOS 10.14+ light/dark mode changes"
   homepage "https://github.com/cormacrelf/dark-notify"
-  url "https://github.com/cormacrelf/dark-notify/releases/download/v0.0.1/dark-notify-v0.0.1.tar.gz"
-  sha256 "64589df7a30d599c7089abe95e386632581a102e95fd3b5a36db5fb18428252d"
-  version "v0.0.1"
+  url "https://github.com/cormacrelf/dark-notify/releases/download/v0.1.0/dark-notify-v0.1.0.tar.gz"
+  sha256 "bf84daa1dc6eaaee1bd55f89c3d1ffa72a4199cc8f6113fe57c85e3e2e950d81"
+  version "0.1.0"
 
   def install
     bin.install "dark-notify"
