9e3cad75253ecc8d55a295a6cf933f33042553e6
Cormac Relf <web@cormacrelf.net>
2020-09-17 20:15:07 +1000

    initial commit

---
+++ Formula/dark-notify.rb
@@ -0,0 +1,11 @@
+class DarkNotify < Formula
+  desc "Watcher for macOS 10.14+ light/dark mode changes"
+  homepage "https://github.com/cormacrelf/dark-notify"
+  url "https://example.com/nothing.tar.gz"
+  sha256 "nonsense"
+  version "0.0.0"
+
+  def install
+    bin.install "dark-notify"
+  end
+end
